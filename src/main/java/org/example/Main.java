6ec9369de07fb0353fd05e61afbee8004c746a4c
1261362 <1261362@bdeb.qc.ca>
2024-06-06 14:21:43 -0400

    Update Main.java
    nouveau commentaire comit 2.2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -15,6 +15,7 @@ public class Main {
             System.out.println("Voici un modification ajouter par l'utilisateur");
             System.out.println("nouveau commit");
             System.out.println("nouveau commit sur la branche_2");
+            System.out.println("nouveau commit sur la branche_2.2");
 
 
 
